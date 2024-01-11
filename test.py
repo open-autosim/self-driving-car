6957efebc5249c7ae072a60bec58bfe2f11cc9c7
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-01-11 10:33:53 -0500

    adding socket

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import socket
 import json
-import select
 
 class CarClient:
     def __init__(self, host='127.0.0.1', port=8080):
@@ -15,35 +14,29 @@ class CarClient:
         self.client_socket.sendall(data.encode())  # Send the data
 
     def receive_data(self):
-        ready = select.select([self.client_socket], [], [], 5)  # Wait for up to 5 seconds
-        if ready[0]:
-            data_size = int(self.client_socket.recv(1024).decode())  # Receive the size
-            data = self.client_socket.recv(data_size).decode()  # Receive the data
-            return data
-        else:
-            return "No data received within the timeout."
+        data_size = int(self.client_socket.recv(1024).decode())  # Receive the size
+        data = self.client_socket.recv(data_size).decode()  # Receive the data
+        return data
 
     def close_connection(self):
         self.client_socket.close()
 
 # Example usage
 client = CarClient()
+client.send_data("Hello from Python!")
+response = client.receive_data()
+print("Received from server:", response)
 
-# response = client.receive_data()
-# print("Received from server:", response)
 
-# Create JSON data with field controls and inner fields [forward, reverse, left, right]
-data = {
-    "controls": {
-        "forward": "true",
-        "reverse": "false",
-        "left": "false",
-        "right": "false"
-    }
-}
 
-# Convert the data to JSON string
-json_data = json.dumps(data)
+
+
+# Example usage
+
 client.send_data(json_data)
-
+response = client.receive_data()
+print("Received from server:", response)
 client.close_connection()
+
+
+
